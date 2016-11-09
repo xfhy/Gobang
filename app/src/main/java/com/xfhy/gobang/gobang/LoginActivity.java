980afc06bbfcda7ff29cd2310a5729434f2e4331
xfhy <1319504383@qq.com>
2016-11-09 12:14:37 +0800

    完成第一次进入卡片式滑动,进入界面加入模拟广告

---
+++ app/src/main/java/com/xfhy/gobang/gobang/LoginActivity.java
@@ -0,0 +1,72 @@
+package com.xfhy.gobang.gobang;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+
+/**
+ * 2016年11月8日22:48:44
+ * 这是主界面
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);  //这个界面是开始界面
+
+        //安卓全屏代码
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                try {
+                    Thread.sleep(2000);   //开始界面上全屏显示   2秒
+                    //判断是否是第一次进入app
+                    if(isRunStart(LoginActivity.this)){
+                        //进入卡片页面
+                        Intent intent = new Intent(LoginActivity.this,
+                                GlideActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }else{
+                        //直接进入主界面
+                        Intent intent1 = new Intent(LoginActivity.this,
+                                StartScreenActivity.class);
+                        startActivity(intent1);
+                        finish();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    /**
+     * 判断是否是第一次进入app
+     * @param context
+     * @return
+     */
+    public boolean isRunStart(Context context){
+        SharedPreferences sp = context.getSharedPreferences("shared", MODE_PRIVATE);
+        boolean isRunStart = sp.getBoolean("isRunStart", true);
+        SharedPreferences.Editor editor = sp.edit();
+        if(isRunStart){  //如果是第一次进入会是true
+            //写入文件
+            editor.putBoolean("isRunStart", false);   //则写入文件false(下次就不是了)
+            editor.commit();
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
--- app/src/main/java/com/xfhy/gobang/gobang/StartScreenActivity.java
+++ app/src/main/java/com/xfhy/gobang/gobang/StartScreenActivity.java
@@ -3,22 +3,28 @@ package com.xfhy.gobang.gobang;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.xfhy.gobang.gobang.model.GamePattern;
+import net.youmi.android.AdManager;
+import net.youmi.android.normal.banner.*;
 
 /**
  * 2016年11月5日16:22:35
  * 开始界面
+ * 发布ID:ce5360bb2cb842a5
+ * 应用密匙:0e7c7db390b22793
  */
 public class StartScreenActivity extends AppCompatActivity {
 
-    private Button startgamebymanandmachine = null;
-    private Button startgamebymanandman = null;
-    private Button helpbtn = null;
-    private Button aboutbtn = null;
-    private Button exitbtn = null;
+    private Button startgamebymanandmachine = null;   //人机按钮
+    private Button startgamebymanandman = null;       //人人按钮
+    private Button helpbtn = null;                    //帮助按钮
+    private Button aboutbtn = null;                   //关于按钮
+    private Button exitbtn = null;                    //退出按钮
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +41,52 @@ public class StartScreenActivity extends AppCompatActivity {
         helpbtn.setOnClickListener(new HelpListener());
         aboutbtn.setOnClickListener(new AboutListener());
         exitbtn.setOnClickListener(new ExitListener());
+        /*
+        参数说明：
+appId 和 appSecret 分别为应用的发布 ID 和密钥，由有米后台自动生成，通过在有米后台 > 应用详细信息 可以获得。
+isTestModel : 是否开启测试模式，true 为是，false 为否。（上传有米审核及发布到市场版本，请设置为 false）
+isEnableYoumiLog: 是否开启有米的Log输出，默认为开启状态
+上传到有米主站进行审核时，务必开启有米的Log，这样才能保证通过审核
+开发者发布apk到各大市场的时候，强烈建议关闭有米的Log
+         */
+        AdManager.getInstance(StartScreenActivity.this).
+                init("ce5360bb2cb842a5", "0e7c7db390b22793",
+        false, true);
+        // 获取广告条
+        View bannerView = BannerManager.getInstance(StartScreenActivity.this)
+              .getBannerView(
+                      new net.youmi.android.normal.banner.BannerViewListener(){
+
+                          /**
+                           * 请求广告成功
+                           */
+                          @Override
+                          public void onRequestSuccess() {
+                              Log.d("xfhy","请求广告成功");
+                          }
+
+                          /**
+                           * 切换广告条
+                           */
+                          @Override
+                          public void onSwitchBanner() {
+                              Log.d("xfhy","切换广告条");
+                          }
+
+                          /**
+                           * 请求广告失败
+                           */
+                          @Override
+                          public void onRequestFailed() {
+                              Log.d("xfhy","Log.d(\"xfhy\",\"切换广告条\");");
+                          }
+                      });
+
+        // 获取要嵌入广告条的布局
+        LinearLayout bannerLayout = (LinearLayout) findViewById(R.id.ll_banner);
+
+        // 将广告条加入到布局中
+        bannerLayout.addView(bannerView);
     }
 
     //开始游戏  按钮监听器
